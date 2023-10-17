d42747a7e9076bc9e2a6cc99e35d2322c6b27363
Сергей Ангелов <sergejangelov@MacBook-Air-Sergej.local>
2023-10-17 14:33:55 +0300

    UIHW1.2

--- UIHWs/ViewController.swift
+++ UIHWs/ViewController.swift
@@ -9,142 +9,62 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    //- MARK: Создание лейблов и кнопок
+    //MARK: - Создаем лейбл и кнопку
     
-    private let greetingLabel = UILabel()
-    private let resultAddLabel = UILabel()
-    private let resultGuessLabel = UILabel()
+    private let startButton = UIButton()
+    private let helloLabel = UILabel()
     
     private func createViews() {
         
-        //Кнопка сложения
-        let addButton = UIButton(frame: CGRect(x: 35, y: 410, width: 150, height: 150))
-        addButton.backgroundColor = .red
-        addButton.setTitle("Сложение", for: .normal)
-        addButton.layer.cornerRadius = 40
-        addButton.addTarget(self, action: #selector(addButtonAction), for: .touchUpInside)
+        //Кнопка запуска
+        startButton.frame = CGRect(x: 98, y: 440, width: 200, height: 200)
+        startButton.setTitle("Начать", for: .normal)
+        startButton.backgroundColor = .red
+        startButton.layer.cornerRadius = 40
+        startButton.addTarget(self, action: #selector(alertAction), for: .touchUpInside)
         
-        //Кнопка угадывания
-        let guessButton = UIButton(frame: CGRect(x: 210, y: 410, width: 150, height: 150))
-        guessButton.backgroundColor = .red
-        guessButton.setTitle("Угадай число", for: .normal)
-        guessButton.layer.cornerRadius = 40
-        guessButton.addTarget(self, action: #selector(guessButtonAction), for: .touchUpInside)
+        //Лейбл результата
+        helloLabel.frame = CGRect(x: 98, y: 300, width: 200, height: 40)
+        helloLabel.backgroundColor = .white
+        helloLabel.textColor = .black
+        helloLabel.textAlignment = .center
         
-        //Приветственный лейбл
-        greetingLabel.frame = CGRect(x: 0, y: 70, width: 397, height: 40)
-        greetingLabel.backgroundColor = .white
-        greetingLabel.textColor = .black
-        greetingLabel.textAlignment = .center
-        
-        //Лейбл результата сложения
-        resultAddLabel.frame = CGRect(x: 35, y: 350, width: 150, height: 50)
-        resultAddLabel.backgroundColor = .gray
-        resultAddLabel.textColor = .black
-        resultAddLabel.text = "Результат: "
-        
-        //Лейбл результата угадывания
-        resultGuessLabel.frame = CGRect(x: 210, y: 350, width: 150, height: 50)
-        resultGuessLabel.backgroundColor = .gray
-        resultGuessLabel.textColor = .black
-        resultGuessLabel.text = "Результат: "
-        
-        self.view.addSubview(greetingLabel)
-        self.view.addSubview(resultAddLabel)
-        self.view.addSubview(resultGuessLabel)
-        self.view.addSubview(addButton)
-        self.view.addSubview(guessButton)
+        self.view.addSubview(startButton)
+        self.view.addSubview(helloLabel)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         //Фон
         view.backgroundColor = .darkGray
+        
         createViews()
     }
     
-    // - MARK: Добавляем действия для кнопок
+    //MARK: - Добавляем действие для кнопки
     
-    @objc func addButtonAction(sender: UIButton) {
+    @objc func alertAction(sender: UIButton) {
         
-        addTwoNumbers()
-        
-        func addTwoNumbers() {
-            
-            //Создаем алерт для ввода цифр которые необходимо сложить
-            let addAlertController = UIAlertController(title: "Пожалуйста", message: "Введите числа", preferredStyle: .alert)
-            addAlertController.addTextField { UITextField in
-                UITextField.placeholder = "Число 1"
-            }
-            
-            addAlertController.addTextField { UITextField in
-                UITextField.placeholder = "Число 2"
-            }
-            
-            //Создаем действие сложение 2 чисел
-            let actionAdd = UIAlertAction(title: "Сложить", style: .default) { _ in
-                let numberOne = Int((addAlertController.textFields?[0].text!)!)
-                let numberTwo = Int((addAlertController.textFields?[1].text!)!)
-                self.resultAddLabel.text! = String((numberOne ?? 0) + (numberTwo ?? 0))
-            }
-            
-            addAlertController.addAction(actionAdd)
-            self.present(addAlertController, animated: true)
-        }
-    }
-    
-    @objc func guessButtonAction(sender: UIButton) {
-        
-        //Создаем алерт для ввода цифры которую будем угадывать
-        let number = Int.random(in: 1...10)
-        print(number)
-        let guessAlertController = UIAlertController(title: "Угадайте число", message: "Загадайте от 1 до 10", preferredStyle: .alert)
-        guessAlertController.addTextField { UITextField in
-            UITextField.placeholder = "1 - 10"
+       //Создаем алерт для ввода слова
+        let alertInputWord = UIAlertController(title: "Пожалуйста", message: "Введите leohl", preferredStyle: .alert)
+        alertInputWord.addTextField { UITextField in
+            UITextField.placeholder = "leohl"
         }
         
-        //Создаем действие угадывания числа
-        let actionGuess = UIAlertAction(title: "OK", style: .default) { _ in
-            if let inputedNumber = guessAlertController.textFields?.first?.text {
-                print(inputedNumber)
-                if number == Int(inputedNumber) {
-                    self.resultGuessLabel.text = ":)"
-                } else {
-                    self.resultGuessLabel.text = ":("
-                }
-            }
-        }
+        //Создаем конструктор
         
-        guessAlertController.addAction(actionGuess)
-        self.present(guessAlertController, animated: true)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        alertRegistration()
-    }
-    // - MARK: Создаем приветственный алерт
-    
-    private func alertRegistration() {
-        
-        let alertControllerRegistration = UIAlertController(title: "Регистрация", message: "Введите ваше ФИО", preferredStyle: .alert)
-        alertControllerRegistration.addTextField { UITextField in
-            UITextField.placeholder = "ФИО"
-        }
-        
-        //Создаем варианты при наличии и отсутствии name
-        let actionRegistration = UIAlertAction(title: "ОК", style: .default) { _ in
-            guard let name = alertControllerRegistration.textFields?.first?.text?.capitalized else { return }
-            if name.isEmpty {
-                self.greetingLabel.text = "Приветствую, Гость!"
+        let actionWord = UIAlertAction(title: "Ok", style: .default) { _ in
+            if let word = alertInputWord.textFields?[0],
+               word.text == "leohl" {
+                self.helloLabel.text = "hello"
             } else {
-                self.greetingLabel.text = "Приветствую, " + name + "!"
+                self.helloLabel.text = "Ошибка!"
             }
         }
-        
-        alertControllerRegistration.addAction(actionRegistration)
-        self.present(alertControllerRegistration, animated: true)
+        alertInputWord.addAction(actionWord)
+        self.present(alertInputWord, animated: true)
     }
 }
-
-
+    
+    
